8c4b8aaf9b025e2aa488b2cc5cc1b317a5588a5c
Jonathan Wood <jwood@wintellect.com>
2016-03-12 15:27:46 -0500

    Get canopy project running

---
+++ FSharpDemo.EndToEndTest/Program.fs
@@ -0,0 +1,15 @@
+﻿open canopy
+open runner
+open System
+
+start chrome
+
+"Test canopy" &&& fun _ ->
+    url "http://localhost:48213/"
+
+run()
+
+printfn "press [enter] to exit"
+System.Console.ReadLine() |> ignore
+
+quit()
